57b9cfa44dac4f83a421ccbca85d182c4573ceeb
Merve Seçinti Şahan <secintimerve27@gmail.com>
2023-06-15 23:38:09 +0300

    15-06-2023

--- src/test/java/techproed/day12_WindowsHandles/C02_BasicAuthentication.java
+++ src/test/java/techproed/day12_WindowsHandles/C02_BasicAuthentication.java
@@ -23,14 +23,25 @@ public class C02_BasicAuthentication extends TestBase {
         bekle(3);
 
         //Başarılı bir girişten sonra Congratulations! yazısının çıktığını doğrulayın.
-        WebElement test= driver.findElement(By.xpath("//p"));
+        WebElement test = driver.findElement(By.xpath("//p"));
         Assert.assertTrue(test.getText().contains("Congratulations!"));
 
 
         //Elemental Selenium yazısına tıklayalım
-        driver.findElement(By.xpath("(//a)[2]"));
+        driver.findElement(By.xpath("(//a)[2]")).click();
+
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString()); //Açılan yeni pencereye geçtik
 
         // Başlığın Elemental Selenium olduğunu doğrulayın
+        System.out.println("Sayfa Başlığı" + driver.getTitle());
+        Assert.assertNotEquals("Elemental Selenium", driver.getTitle());
 
+        //DDM den java seçilim
+        WebElement ddm = driver.findElement(By.xpath("//select"));
+        /*
+        Select select = new Select(ddm);
+        select.selectByVisibleText("Java");
+         */
+        selectVisibleText(ddm, "Java"); //--> Method ile
     }
 }
